4122e9f87299f52c94428943d8d67401212ca904
Tanish18y <y.tanish06@gmail.com>
2024-10-14 07:48:07 +0530

    Hooks

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Login from "./Login"
+import Register from "./Register"
+import NoMatch from "./NoMatch"
+import Dashboard from "./Dashboard"
+function App(){
+    return <Login/>
+}
+export default App
---
+++ src/NoMatch.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function NoMatch() {
+  return (
+    <h1 className='text-danger'>
+        Page Not Found! ERROR 404...
+    </h1>
+    
+  )
+}
+
+export default NoMatch
